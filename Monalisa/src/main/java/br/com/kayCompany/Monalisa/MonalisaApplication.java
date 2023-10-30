10aab7d8bdb690acc65a78f0595728eca2e72389
kayqueln <kayqueln@gmail.com>
2023-10-30 20:56:49 -0300

    implementação da rota health e tags do swagger

--- Monalisa/src/main/java/br/com/kayCompany/Monalisa/MonalisaApplication.java
+++ Monalisa/src/main/java/br/com/kayCompany/Monalisa/MonalisaApplication.java
@@ -1,8 +1,5 @@
 package br.com.kayCompany.Monalisa;
 
-import br.com.kayCompany.Monalisa.controller.ContaController;
-import br.com.kayCompany.Monalisa.domain.fatura.Fatura;
-import com.sun.tools.jconsole.JConsoleContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
